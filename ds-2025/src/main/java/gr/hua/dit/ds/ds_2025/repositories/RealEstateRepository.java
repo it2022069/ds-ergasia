a5e530471a12f1931ecc1a79f8e8104881656063
it2022069 <it2022069@hua.gr>
2025-01-08 13:08:38 +0200

    Delete ds-2025 directory

--- ds-2025/src/main/java/gr/hua/dit/ds/ds_2025/repositories/RealEstateRepository.java
+++
@@ -1,9 +0,0 @@
-package gr.hua.dit.ds.ds_2025.repositories;
-
-import gr.hua.dit.ds.ds_2025.entities.RealEstate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RealEstateRepository extends JpaRepository<RealEstate, Integer> {
-}
